c1724d0b49dcf53a170a6b2d86de324cd3e07298
Will LaBranche <wlabranche@gmail.com>
2014-04-29 18:44:52 -0700

    Built reject function

--- src/underbar.js
+++ src/underbar.js
@@ -92,6 +92,9 @@ var _ = {};
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    return _.filter(collection, function(el){
+      return !test(el);
+    });
   };
 
   // Produce a duplicate-free version of the array.
