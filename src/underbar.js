31cce9007a242a81c605f3b2a3869b0b059daae9
Hack Reactor Students <accounts@hackreactor.com>
2014-06-02 20:25:02 -0700

    built _.filter

--- src/underbar.js
+++ src/underbar.js
@@ -86,9 +86,9 @@ var _ = {};
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
     var result = [];
-    _.each(collection, function(el){
-      if (test(el)){
-        result.push(el);
+    _.each(collection, function(item){
+      if(test(item)){
+        result.push(item);
       }
     });
     return result;
