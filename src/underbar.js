0a3e4079c09e657b1463002dacb81edd338cf57c
Hack Reactor Students <accounts@hackreactor.com>
2014-06-02 20:08:57 -0700

    finished _.last

--- src/underbar.js
+++ src/underbar.js
@@ -39,8 +39,14 @@ var _ = {};
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
-    if (n > array.length){n = array.length;}
-    return  n === undefined ? array[array.length - 1] : array.slice(array.length - n);
+    if( n > array.length){
+      n = array.length;
+    }
+    if (n === undefined){
+      return array[array.length - 1];
+    }else{
+      return array.slice(array.length - n);
+    }
   };
 
   // Call iterator(value, key, collection) for each element of collection.
