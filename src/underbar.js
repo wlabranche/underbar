3efa9a0f72922107d062e8f200273c38ed9dd410
Hack Reactor Students <accounts@hackreactor.com>
2014-06-02 20:44:45 -0700

    built _.reject using filter

--- src/underbar.js
+++ src/underbar.js
@@ -98,8 +98,8 @@ var _ = {};
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
-    return _.filter(collection, function(el){
-      return !test(el);
+    return _.filter(collection, function(item){
+      return !test(item);
     });
   };
 
